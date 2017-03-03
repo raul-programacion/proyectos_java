9022c7a3c6ca1cba602a8cda69bf81baaa266034
adminprofe <adminprofe@lince>
2017-03-03 10:50:46 +0100

    Relacion Persona prestamo Libro

--- Practica30/src/practica30/Practica30.java
+++ Practica30/src/practica30/Practica30.java
@@ -16,6 +16,26 @@ public class Practica30 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Persona socio = new Persona("18999888","Raul","777888999","Quijote");
+        
+        Libro book = new Libro();
+        
+       book.setLibroIsbn("111777555");
+       book.setLibroTitulo("Mio Cid");
+       
+       socio.setLibroList(book);
+        
+        
+        System.out.print("Dni: ");
+        System.out.println(socio.getPersonaDni());
+        System.out.print("Nombre: ");
+        System.out.println(socio.getPersonaNombre());
+        System.out.println("Libros");
+        System.out.println("======================");
+        socio.getLibroList();
+        
+        
     }
     
 }
