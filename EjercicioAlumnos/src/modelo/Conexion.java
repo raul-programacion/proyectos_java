b9529bbecb2214d6ee9e8dfe7d80aa849bfbea1f
adminprofe <adminprofe@lince>
2017-05-12 09:29:38 +0200

    Proyecto JavaFX Gestión Alumnos

---
+++ EjercicioAlumnos/src/modelo/Conexion.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author adminprofe
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    
+    private Connection connection;
+	private String url = "jdbc:mysql://localhost/db_alumnos";
+	private String usuario = "root";
+	private String contrasena = "";
+	
+	public Connection getConnection() {
+		return connection;
+	}
+	public void setConnection(Connection connection) {
+		this.connection = connection;
+	}
+	
+	public void establecerConexion(){
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(url, usuario, contrasena);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void cerrarConexion(){
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}	
+    
+    
+}
